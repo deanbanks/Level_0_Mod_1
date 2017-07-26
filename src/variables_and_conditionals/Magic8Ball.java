491d45fd902e9e672121fa8762b4e802c09695d5
deanbanks <dean.banks@jointheleague.org>
2017-07-25 18:31:35 -0700

    Add files via upload

--- src/variables_and_conditionals/Magic8Ball.java
+++ src/variables_and_conditionals/Magic8Ball.java
@@ -2,16 +2,21 @@ package variables_and_conditionals;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below….
-
+public static void main(String[] args) {
+	
     // 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
-    // 3. Print out this variable
-
+	Random randy = new Random();
+	int randNum = randy.nextInt(6);
+	
+	// 3. Print out this variable
+	System.out.println(randNum);
     // 4. Get the user to enter a question for the 8 ball
-
+String answer = JOptionPane.showInputDialog("Are you tired");
     // 5. If the random number is 0
 
     // -- tell the user "Yes"
@@ -23,7 +28,7 @@ public class Magic8Ball {
     // 7. If the random number is 2
 
     // -- tell the user "Maybe you should ask Google?"
-
+}
     // 8. If the random number is 3
 
     // -- write your own answer
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -8,13 +8,24 @@ import javax.swing.JOptionPane;
  * your program should say "June's superpower is writing recipes".
  */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // 1. Save the superpower for each person in a variable.
-
-        // 2. Ask the user to enter a name. Store their answer in a variable.
-
-        // 3. Show the superpower in a pop-up, depending on the name entered. 
-
-    }
+		// 1. Save the superpower for each person in a variable.
+		String deen = "fly";
+		String kyren = "invisable";
+		String amaan = "run fast";
+		String matin = "time travel";
+		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String answer = JOptionPane.showInputDialog("enter a name");
+		// 3. Show the superpower in a pop-up, depending on the name entered.
+		if (answer.equals("deen")) {
+			JOptionPane.showMessageDialog(null, deen);
+		} else if (answer.equals("matin")) {
+			JOptionPane.showMessageDialog(null, matin);
+		} else if (answer.equals("kyren")) {
+			JOptionPane.showMessageDialog(null, kyren);
+		} else if (answer.equals("amaan")) {
+			JOptionPane.showMessageDialog(null, amaan);
+		}
+	}
 }
